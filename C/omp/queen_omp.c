7dc0360f5abfc9d170864c4828a3aeb1d0d30216
jonas <jonas.bonnaudet@gmail.com>
2024-11-18 18:32:22 +0100

    time in double

--- C/omp/queen_omp.c
+++ C/omp/queen_omp.c
@@ -1,136 +1,117 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <time.h>
-#include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int size, no;
 unsigned long sum_num_solutions = 0;
 unsigned long sum_iterations = 0;
 
-bool queenAt(int row, int col, int queens[size])
-{
-    return queens[col] == row;
+bool queenAt(int row, int col, int queens[size]) { return queens[col] == row; }
+
+void printBoard(int queens[size]) {
+  for (int x = 0; x < size; x++) {
+    for (int y = 0; y < size; y++) {
+      if (queenAt(x, y, queens)) {
+        putchar('Q');
+      } else {
+        putchar('.');
+      }
+    }
+    putchar('\n');
+  }
+  puts("==============");
 }
 
-void printBoard(int queens[size])
-{
-    for (int x = 0; x < size; x++)
-    {
-        for (int y = 0; y < size; y++)
-        {
-            if (queenAt(x, y, queens))
-            {
-                putchar('Q');
-            }
-            else
-            {
-                putchar('.');
-            }
-        }
-        putchar('\n');
+bool is_legal(int row, int col, int queens[size]) {
+  // always check to the left
+  // same row
+  // diag up
+  // diag down
+
+  // .Q.
+  // ...
+  // Q..
+  // row = 2
+  // col = 1
+  // y = 0
+  for (int y = 0; y < col; y++) {
+    if (queens[y] == row) {
+      return false;
     }
-    puts("==============");
-}
 
-bool is_legal(int row, int col, int queens[size])
-{
-    // always check to the left
-    // same row
-    // diag up
-    // diag down
-
-    // .Q.
-    // ...
-    // Q..
-    // row = 2
-    // col = 1
-    // y = 0
-    for (int y = 0; y < col; y++)
-    {
-        if (queens[y] == row)
-        {
-            return false;
-        }
-
-        if (queens[y] == row - col + y)
-        {
-            return false;
-        }
-
-        if (queens[y] == row + col - y)
-        {
-            return false;
-        }
+    if (queens[y] == row - col + y) {
+      return false;
     }
-    return true;
+
+    if (queens[y] == row + col - y) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // init to col 0
 // row 1234...
 // if found col++ else col-- and set to -1 (for printboard)
-void resolve(int col, int queens[size], unsigned long *num_solutions, unsigned long *iterations)
-{
-    if (col == size)
-    {
-        // printBoard(queens);
-        (*num_solutions)++;
-        return;
-    }
+void resolve(int col, int queens[size], unsigned long *num_solutions,
+             unsigned long *iterations) {
+  if (col == size) {
+    // printBoard(queens);
+    (*num_solutions)++;
+    return;
+  }
 
-    for (int row = 0; row < size; row++)
-    {
-        (*iterations)++;
-        queens[col] = row;
-        if (is_legal(row, col, queens))
-        {
-            resolve(col + 1, queens, num_solutions, iterations);
-        }
+  for (int row = 0; row < size; row++) {
+    (*iterations)++;
+    queens[col] = row;
+    if (is_legal(row, col, queens)) {
+      resolve(col + 1, queens, num_solutions, iterations);
     }
-    queens[col] = no;
+  }
+  queens[col] = no;
 }
 
-void init_resolve(int row)
-{
+void init_resolve(int row) {
 
-    int queens[size]; // [col] = row
-    unsigned long num_solutions = 0;
-    unsigned long iterations = 1;
-    memset(&queens, no, size * sizeof(int));
+  int queens[size]; // [col] = row
+  unsigned long num_solutions = 0;
+  unsigned long iterations = 1;
+  memset(&queens, no, size * sizeof(int));
 
-    queens[0] = row;
+  queens[0] = row;
 
-    resolve(1, queens, &num_solutions, &iterations);
+  resolve(1, queens, &num_solutions, &iterations);
 
-    #pragma omp critical
-    {
-        sum_num_solutions += num_solutions;
-        sum_iterations += iterations;
-    }
+#pragma omp critical
+  {
+    sum_num_solutions += num_solutions;
+    sum_iterations += iterations;
+  }
 }
 
-int main(int argc, char* argv[])
-{
-    if(argc == 2){
-        size = atoi(argv[1]);
-    }else{
-        size = 8;
-    }
-    no = 2 * size;
-    printf("using size = %i\n", size);
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    size = atoi(argv[1]);
+  } else {
+    size = 8;
+  }
+  no = 2 * size;
+  printf("using size = %i\n", size);
 
-    clock_t start, end;
-    double cpu_time_used;
-    start = omp_get_wtime();
+  double start, end;
+  double cpu_time_used;
+  start = omp_get_wtime();
 
 #pragma omp parallel for
-    for (int i = 0; i < size; i++)
-    {
-        init_resolve(i);
-    }
-    end = omp_get_wtime();
-    cpu_time_used = ((double)(end - start));
+  for (int i = 0; i < size; i++) {
+    init_resolve(i);
+  }
+  end = omp_get_wtime();
+  cpu_time_used = ((double)(end - start));
 
-    printf("%lu solutions\n%lu iterations\ntook %f seconds\n", sum_num_solutions, sum_iterations, cpu_time_used);
+  printf("%lu solutions\n%lu iterations\ntook %f seconds\n", sum_num_solutions,
+         sum_iterations, cpu_time_used);
 }
--- C/single_thread/queen.c
+++ C/single_thread/queen.c
@@ -1,117 +1,99 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <time.h>
-#include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int size, no;
 int *queens; // [col] = row
 unsigned long num_solutions = 0;
 unsigned long iterations = 0;
 
-bool queenAt(int row, int col)
-{
-    return queens[col] == row;
+bool queenAt(int row, int col) { return queens[col] == row; }
+
+void printBoard() {
+  for (int x = 0; x < size; x++) {
+    for (int y = 0; y < size; y++) {
+      if (queenAt(x, y)) {
+        putchar('Q');
+      } else {
+        putchar('.');
+      }
+    }
+    putchar('\n');
+  }
+  puts("==============");
 }
 
-void printBoard()
-{
-    for (int x = 0; x < size; x++)
-    {
-        for (int y = 0; y < size; y++)
-        {
-            if (queenAt(x, y))
-            {
-                putchar('Q');
-            }
-            else
-            {
-                putchar('.');
-            }
-        }
-        putchar('\n');
+bool is_legal(int row, int col) {
+  // always check to the left
+  // same row
+  // diag up
+  // diag down
+
+  // .Q.
+  // ...
+  // Q..
+  // row = 2
+  // col = 1
+  // y = 0
+  for (int y = 0; y < col; y++) {
+    if (queens[y] == row) {
+      return false;
     }
-    puts("==============");
-}
 
-bool is_legal(int row, int col)
-{
-    // always check to the left
-    // same row
-    // diag up
-    // diag down
-
-    // .Q.
-    // ...
-    // Q..
-    // row = 2
-    // col = 1
-    // y = 0
-    for (int y = 0; y < col; y++)
-    {
-        if (queens[y] == row)
-        {
-            return false;
-        }
-
-        if (queens[y] == row - col + y)
-        {
-            return false;
-        }
-
-        if (queens[y] == row + col - y)
-        {
-            return false;
-        }
+    if (queens[y] == row - col + y) {
+      return false;
     }
-    return true;
+
+    if (queens[y] == row + col - y) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // init to col 0
 // row 1234...
 // if found col++ else col-- and set to -1 (for printboard)
-void resolve(int col)
-{
-    if (col == size)
-    {
-        // printBoard();
-        num_solutions++;
-        return;
-    }
+void resolve(int col) {
+  if (col == size) {
+    // printBoard();
+    num_solutions++;
+    return;
+  }
 
-    for (int row = 0; row < size; row++)
-    {
-        iterations++;
-        queens[col] = row;
-        if (is_legal(row, col))
-        {
-            resolve(col + 1);
-        }
+  for (int row = 0; row < size; row++) {
+    iterations++;
+    queens[col] = row;
+    if (is_legal(row, col)) {
+      resolve(col + 1);
     }
-    queens[col] = no;
+  }
+  queens[col] = no;
 }
 
-int main(int argc, char* argv[])
-{
-    if(argc == 2){
-        size = atoi(argv[1]);
-    }else{
-        size = 8;
-    }
-    no = 2 * size;
-    printf("using size = %i\n", size);
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    size = atoi(argv[1]);
+  } else {
+    size = 8;
+  }
+  no = 2 * size;
+  printf("using size = %i\n", size);
 
-    clock_t start, end;
-    double cpu_time_used;
-    start = omp_get_wtime();
+  double start, end;
+  double cpu_time_used;
+  start = omp_get_wtime();
 
-    queens = malloc(size * sizeof(int));
-    memset(queens, no, size * sizeof(int));
-    
-    resolve(0);
-    
-    end = omp_get_wtime();
-    cpu_time_used = ((double)(end - start));
-    printf("%lu solutions\n%lu iterations\ntook %f seconds\n", num_solutions, iterations, cpu_time_used);
+  queens = malloc(size * sizeof(int));
+  memset(queens, no, size * sizeof(int));
+
+  resolve(0);
+
+  end = omp_get_wtime();
+  cpu_time_used = ((double)(end - start));
+  printf("%lu solutions\n%lu iterations\ntook %f seconds\n", num_solutions,
+         iterations, cpu_time_used);
 }
